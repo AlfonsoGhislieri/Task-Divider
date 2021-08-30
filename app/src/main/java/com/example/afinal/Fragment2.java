e6a933f51a0103a53c99cd7e22aca5d42ec90ee5
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2021-08-30 17:05:24 +0200

    first

--- app/src/main/java/com/example/afinal/Fragment2.java
+++ app/src/main/java/com/example/afinal/Fragment2.java
@@ -13,10 +13,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class Fragment2 extends Fragment implements View.OnClickListener {
     @RequiresApi(api = Build.VERSION_CODES.O)
